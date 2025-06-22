01e3c1cb588c848d4b900e34b9bf12ff7fbdd46f
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-06-22 09:30:52 +0545

    ignore jest.config.js from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,11 @@ export default defineConfig([
         },
     },
     {
-        ignores: ["dist", "node_modules", "eslint.config.mjs"],
+        ignores: [
+            "dist",
+            "node_modules",
+            "eslint.config.mjs",
+            "jest.config.js",
+        ],
     },
 ]);
