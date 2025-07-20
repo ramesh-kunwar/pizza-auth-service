e4e03bddaeb2023bdf66180b0e63ca64fa8cae52
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-07-20 13:33:11 +0545

    Add test for tenant 201 status code

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, { NextFunction, Request, Response } from "express";
 import logger from "./config/logger";
 import { HttpError } from "http-errors";
 import authRouter from "./routes/auth";
+import tenantRouter from "./routes/tenant";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -21,6 +22,7 @@ app.get("/", async (req, res) => {
 });
 
 app.use("/auth", authRouter);
+app.use("/tenants", tenantRouter);
 // global error handler
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
