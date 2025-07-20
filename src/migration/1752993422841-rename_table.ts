f4695a554ce3bd9a5d6f394faa9d590e60b651d9
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-07-20 12:43:58 +0545

    Rename table names  users and refreshTokens

---
+++ src/migration/1752993422841-rename_table.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class RenameTable1752993422841 implements MigrationInterface {
+    name = "RenameTable1752993422841";
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        //
+        await queryRunner.query(
+            `ALTER TABLE "refresh_token" DROP CONSTRAINT "FK_8e913e288156c133999341156ad"`,
+        );
+
+        await queryRunner.renameTable("user", "users");
+        await queryRunner.renameTable("refresh_token", "refreshTokens");
+
+        await queryRunner.query(
+            `ALTER TABLE "refreshTokens" ADD CONSTRAINT "FK_265bec4e500714d5269580a0219" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "refreshTokens" DROP CONSTRAINT "FK_265bec4e500714d5269580a0219"`,
+        );
+
+        await queryRunner.renameTable("users", "user");
+        await queryRunner.renameTable("refreshTokens", "refresh_token");
+    }
+}
