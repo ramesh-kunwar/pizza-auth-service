c43bcc029fe3fc015c337cf8b281fe6cd058f50d
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-07-20 15:28:21 +0545

    Add test for tenant - persist in db and check token

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -1,53 +1,87 @@
-import { DataSource, TableInheritance } from "typeorm";
+import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
 import request from "supertest";
 import app from "../../src/app";
 import { Tenant } from "../../src/entity/Tenant";
+import { ROLSE } from "../../src/constants";
+import createJWKSMock from "mock-jwks";
 
-describe("POST /auth/login", () => {
+describe("POST /tenants", () => {
     let connection: DataSource;
+    let jwks: ReturnType<typeof createJWKSMock>;
+    let adminToken: string;
 
     beforeAll(async () => {
         connection = await AppDataSource.initialize();
+        jwks = createJWKSMock("http://localhost:4000");
     });
 
     beforeEach(async () => {
         await connection.dropDatabase();
         await connection.synchronize();
+        jwks.start();
+
+        adminToken = jwks.token({
+            sub: "1",
+            role: ROLSE.ADMIN,
+        });
     });
 
     afterAll(async () => {
         await connection.destroy();
     });
 
+    afterEach(() => {
+        jwks.stop();
+    });
+
     describe("Given all fields", () => {
         it("should return a 201 status code", async () => {
             const tenantData = {
-                name: "Tenant Name",
-                address: "Tenant Address",
+                name: "Tenant name",
+                address: "Tenant address",
             };
             const response = await request(app)
                 .post("/tenants")
+                .set("Cookie", [`accessToken=${adminToken}`])
                 .send(tenantData);
 
             expect(response.statusCode).toBe(201);
         });
 
-        it("should create tenant in database", async () => {
+        it("should create a tenant in the database", async () => {
             const tenantData = {
-                name: "Tenant Name",
-                address: "Tenant Address",
+                name: "Tenant name",
+                address: "Tenant address",
             };
-            await request(app).post("/tenants").send(tenantData);
 
-            // get data from db
+            await request(app)
+                .post("/tenants")
+                .set("Cookie", [`accessToken=${adminToken}`])
+                .send(tenantData);
+
             const tenantRepository = connection.getRepository(Tenant);
-
             const tenants = await tenantRepository.find();
-
             expect(tenants).toHaveLength(1);
             expect(tenants[0].name).toBe(tenantData.name);
             expect(tenants[0].address).toBe(tenantData.address);
+        });
+
+        it("should return 401 if user is not autheticated", async () => {
+            const tenantData = {
+                name: "Tenant name",
+                address: "Tenant address",
+            };
+
+            const response = await request(app)
+                .post("/tenants")
+                .send(tenantData);
+            expect(response.statusCode).toBe(401);
+
+            const tenantRepository = connection.getRepository(Tenant);
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(0);
         });
     });
 });
